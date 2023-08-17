28dd7125c4cd5e68f3e0a6792a2f75169b90a342
FurryKitten <grvsanya@yandex.ru>
2023-08-17 17:36:14 +0300

    Menu and gamestate in progress

--- Assets/Scripts/GameState.cs
+++ Assets/Scripts/GameState.cs
@@ -1,21 +1,44 @@
-using UnityEngine;
+﻿using UnityEngine;
+using UnityEngine.Events;
 
 public enum State
 {
+    PAUSED,
     TETRIS,
     WALK
 }
 
 public class GameState : MonoBehaviour, IService
 {
+
     public State State { get; private set; }
+    public int CurrentStage { get; private set; } = 0;
+
+    public int Money { get; private set; } = 0;
+    public int MoneyOnTray { get; private set; } = 0;
 
     [SerializeField, Range(2, 5)] private int _stages = 5;
+    [SerializeField] private UnityEvent _onStageChange;
+    [SerializeField] private UnityEvent _onMoneyChange;
 
-    private int _currentStage = 0;
-
+    [ContextMenu("Add stage")]
     public void AddStage()
     {
-        _currentStage++;
+        CurrentStage++;
+        _onStageChange.Invoke();
+    }
+
+    public void SetState(State state)
+    {
+        State = state;
+    }
+
+    /// Money - общие деньги, MoneyOnTray - стоимость еды на подносе
+    /// Пока несем, меняем MoneyOnTray, донесли - зафиксировали в Money
+    public void AddMoney(int money)
+    {
+        Money += money;
+        Money = Money < 0 ? 0 : Money;
+        _onMoneyChange.Invoke();
     }
 }
