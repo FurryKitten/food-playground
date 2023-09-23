2c4857479dad2907d8f3bf3c98e2c68d24104a22
mmay2000 <102404342+mmay2000@users.noreply.github.com>
2023-09-23 22:21:25 +0300

    SandTimer in order

--- Assets/Scripts/UI/OrderScript.cs
+++ Assets/Scripts/UI/OrderScript.cs
@@ -1,11 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class OrderScript : MonoBehaviour
 {
     [SerializeField] Sprite[] _figuresIcons;
+    [SerializeField] Image _SandTimer;
     private Image _image;
 
     private void Start()
@@ -14,7 +16,17 @@ public class OrderScript : MonoBehaviour
     }
     public void ChangeIcon(int index)
     {
-        _image.overrideSprite = _figuresIcons[index];
-        _image.SetNativeSize();
+        if (index < 25)
+        {
+            _SandTimer.enabled = false;
+            _image.enabled = true;
+            _image.overrideSprite = _figuresIcons[index];
+            _image.SetNativeSize();
+        }
+        else
+        {
+            _image.enabled = false;
+            _SandTimer.enabled = true;
+        }
     }
 }
