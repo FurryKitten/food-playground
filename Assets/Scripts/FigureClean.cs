9aa4795a58c5b807af3e38ff5384f4886e1cb2d5
mmay2000 <102404342+mmay2000@users.noreply.github.com>
2023-10-06 00:52:04 +0300

    fix dish crush sound in walk

--- Assets/Scripts/FigureClean.cs
+++ Assets/Scripts/FigureClean.cs
@@ -5,7 +5,8 @@ public class FigureClean : MonoBehaviour
     private void OnTriggerEnter2D(Collider2D collision)
     {
         Destroy(collision.gameObject);
-        if (ServiceLocator.Current.Get<GameState>().State == State.TETRIS)
+        if (ServiceLocator.Current.Get<GameState>().State == State.TETRIS 
+            || ServiceLocator.Current.Get<GameState>().State == State.WALK)
         {
             if(collision.GetComponent<Figure>().Index != 18)
                 ServiceLocator.Current.Get<AudioService>().PlayBreakDish();
